2978a92d8ccaadc876e29304c674407c668e71d1
Lars van Rhijn <larsvanrhijn@gmail.com>
2021-03-22 20:39:14 +0100

    Removed unused stuff

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,24 +8,10 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
     path: "/auth/callback",
     name: "OAuth Authorization",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Authorize.vue")
-  },
-  {
-    path: "/shift",
-    name: "Shift",
-    component: () => import("../views/Shift.vue")
   }
 ];
 
