2f4fcd19c982699e4ebf7ccbad47939dd22e4876
Lars van Rhijn <larsvanrhijn@gmail.com>
2021-03-29 21:01:07 +0200

    Nice date formatting

--- src/components/ShiftCard.vue
+++ src/components/ShiftCard.vue
@@ -2,7 +2,7 @@
   <div class="card">
     <div class="card-header">
       <h2 class="card-title">Shift {{ shift.pk }}</h2>
-      <p class="card-subtitle">From {{ shift.start_date }} to {{ shift.end_date }}</p>
+      <p class="card-subtitle">From {{ start_end_time() }}</p>
     </div>
     <div class="card-body">
       #Orders: {{ shift.num_orders }}
@@ -23,6 +23,26 @@ export default {
   name: "ShiftCard",
   props: {
     shift: Shift,
+  },
+  methods: {
+    start_end_time: function() {
+      if (this.shift.start_date) {
+        const start_date = new Date(this.shift.start_date);
+        const end_date = new Date(this.shift.end_date);
+        const today_date = new Date();
+        if (start_date.getDate() === end_date.getDate() && start_date.getFullYear() === end_date.getFullYear() && start_date.getMonth() === end_date.getMonth()) {
+          if (start_date.getDate() === today_date.getDate() && start_date.getFullYear() === today_date.getFullYear() && start_date.getMonth() === today_date.getMonth()) {
+            return `${start_date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} until ${end_date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`;
+          }
+          return `${start_date.toLocaleDateString()}, ${start_date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} until ${end_date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`;
+        }
+        if (start_date.getDate() === today_date.getDate() && start_date.getFullYear() === today_date.getFullYear() && start_date.getMonth() === today_date.getMonth()) {
+          return `${start_date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} until ${end_date.toLocaleDateString()}, ${end_date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`;
+        }
+        return `${start_date.toLocaleDateString()}, ${start_date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} until ${end_date.toLocaleDateString()}, ${end_date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`;
+      }
+      return "";
+    },
   }
 }
 </script>
